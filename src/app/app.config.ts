2382b1ee4b29e751764def15d1fb5716285e57f3
Ian Barreto <ian.barreto.700@ufrn.edu.br>
2024-07-21 13:39:49 -0300

    Componente título

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,5 +6,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true })
+  ]
 };
--- src/app/components/usuario/list-usuario/list-usuario.component.ts
+++ src/app/components/usuario/list-usuario/list-usuario.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header.component";
 import { MenuComponent } from "../../menu/menu.component";
+import { TituloComponent } from "../../../titulo/titulo.component";
 
 @Component({
   selector: 'app-list-usuario',
   standalone: true,
-  imports: [HeaderComponent, MenuComponent],
+  imports: [HeaderComponent, MenuComponent, TituloComponent],
   templateUrl: './list-usuario.component.html',
   styleUrl: './list-usuario.component.css'
 })
